64b62018327dd55bd767283984007d657ecbd2d8
CT08\CT08 <pcthanh0802@gmail.com>
2023-10-31 13:08:11 +0700

    adjust cors config

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: { origin: '*' } });
+  const app = await NestFactory.create(AppModule, {
+    cors: { origin: ['https://arcelity.vercel.app', '*'] },
+  });
   await app.listen(8000);
 }
 bootstrap();
