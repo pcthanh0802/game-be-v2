fef62f59fa4fa1a340936fd82ff42d3f618f52b4
CT08\CT08 <pcthanh0802@gmail.com>
2023-10-31 12:54:28 +0700

    add cors via app.use

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { cors } from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: true,
-    credentials: true,
-  });
+  app.use('*', cors({ origin: true }));
   await app.listen(8000);
 }
 bootstrap();
