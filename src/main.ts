5eb337ff354bb536af71ac0d555e9e6efd76191b
CT08\CT08 <pcthanh0802@gmail.com>
2023-10-31 13:39:08 +0700

    allow cors for all origins

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    cors: { origin: ['https://arcelity.vercel.app', '*'] },
+    cors: true,
   });
   await app.listen(8000);
 }
