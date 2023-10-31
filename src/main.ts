31371a99fd94406954b60b3cc5485e4d31434051
CT08\CT08 <pcthanh0802@gmail.com>
2023-10-31 09:55:40 +0700

    fix: add origin to cors in main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({origins:['0.0.0.0']});
+  app.enableCors({ origin: ['0.0.0.0'] });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
